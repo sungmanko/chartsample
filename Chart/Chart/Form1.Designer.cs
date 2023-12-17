7d543eacd2a5beeca182eda09dc4f6a31e2e377e
TOSUN <33561581+sungmanko@users.noreply.github.com>
2023-12-17 13:59:54 +0900

    answer
    https://kin.naver.com/qna/detail.naver?d1id=1&dirId=1040102&docId=460425086&mode=answer

---
+++ Chart/Chart/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(23, 51);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Chart/Chart/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 챠트설정
+        /// </summary>
+        private void InitializeChart()
+        {
+            // Chart 설정 초기화
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            // ChartArea 추가
+            ChartArea chartArea = new ChartArea();
+            chart1.ChartAreas.Add(chartArea);
+
+            // Series 추가
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 2;
+            chart1.Series.Add(series);
+
+            // 더미 데이터 추가 (예시)
+            Random random = new Random();
+            for (int year = 1980; year <= 2020; year += 10)
+            {
+                series.Points.AddXY(year, random.Next(100, 500));
+            }
+
+            // X축 설정
+            chartArea.AxisX.Minimum = 1980;
+            chartArea.AxisX.Maximum = 2020;
+            chartArea.AxisX.Interval = 10;
+            chartArea.AxisX.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+
+            // Y축 설정
+            chartArea.AxisY.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+
+            // 제목 및 레이블 설정
+            chartArea.AxisX.Title = "Year";
+            chartArea.AxisY.Title = "Value";
+            chart1.Titles.Add("ChartTitle").Text = "Data by Decades";
+        }
+
+        /// <summary>
+        /// 폼로드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeChart();
+        }
+    }
+}
